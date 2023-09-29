d0ed4200f2aa03602d3024d8a60e560c11774462
hkoov <h.kooverjee1@gmail.com>
2023-09-29 23:06:45 +0200

    sin sweep

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import time
 
 i = 0
 
-#ser1 = serial.Serial('/dev/serial0', 115200, timeout=0.050)
+ser0 = serial.Serial('/dev/ttyACM0', 115200, timeout=0.050)
+ser1 = serial.Serial('/dev/ttyACM1', 115200, timeout=0.050)
 
 while True:
-    angle = 0
+    angle1 = 0
+    angle2 = 0
+    angle3 = 0
 
     if i == 360:
         i = 0
@@ -18,15 +21,20 @@ while True:
 
     i += 1
 
-    angle = frac * 45
+    angle1 = frac * 45
+    angle2 = 30 - frac * 30
+    angle3 = 30 + frac * 30
 
-    angle_str = str(int(angle))
+    angle1_str = str(int(angle1))
+    angle2_str = str(int(angle2))
+    angle3_str = str(int(angle3))
 
-    string1 = angle_str + "," + "0,0," + angle_str + "," + "0,0," + angle_str + "," + "0,0" 
+    string1 = angle1_str + "," + angle2_str + "," + angle3_str + "," + angle1_str + "," + angle2_str + "," + angle3_str + "," + angle1_str + "," + angle2_str + "," + angle3_str
 
     print(string1)
 
-    # ser1.write(string1)
+    ser0.write(str.encode(string1 + "\n"))
+    ser1.write(str.encode(string1 + "\n"))
 
     time.sleep(1/100)
 
